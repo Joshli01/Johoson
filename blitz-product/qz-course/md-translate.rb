d16c0a73c7c6b15dfe1fbf62f2063557f0dc7ed5
howardyang <Howardyang0226@gmail.com>
2021-08-25 17:24:07 +0800

    howard

--- blitz-product/qz-course/md-translate.rb
+++
@@ -1,4 +0,0 @@
-Dir.glob("*.docx") do |my_text_file| # note one extra "*"
-  filename = File.basename(my_text_file, ".docx")
-  system "pandoc -f docx -t markdown '#{filename}.docx' > '#{filename}.md'"
-end
